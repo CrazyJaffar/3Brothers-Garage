03edad29c34efc71c98e0bd06c98199890f001e0
CrazyJaffar <84711098+CrazyJaffar@users.noreply.github.com>
2024-05-01 20:17:51 +0530

    Add files via upload

---
+++ Contact form to email.js
@@ -0,0 +1,22 @@
+function emailSend(){
+
+    var leads = document.getElementById('Leads').value;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "campaignleads62@gmail.com",
+        Password : "6EBB89F49D89F6CF7D48742A0865352BA966",
+        To : 'campaignleads62@gmail.com',
+        From : "campaignleads62@gmail.com",
+        Subject : "Leads",
+        Body : leads
+    }).then(
+      message => {
+        if(message=='OK'){
+            swal("Success", "Leads has been Generated!", "success");
+        }else{
+            swal("Error!", "Error in generating Leads!", "error");
+        }
+      }
+    );
+}
