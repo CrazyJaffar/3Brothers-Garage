3c623b2626cd80d3892012f2830a3f7ff93be7dc
CrazyJaffar <84711098+CrazyJaffar@users.noreply.github.com>
2024-05-02 18:09:26 +0530

    Update Contact form to email.js

--- Contact form to email.js
+++ Contact form to email.js
@@ -1,25 +1,8 @@
-function emailSend(){
+const leads = document.getElementById('Leads');
 
-    var leads = document.getElementById('Leads').value;
+const leads = document.getElementByClassName('form contact')[0];
 
-    var info = "Generated Leads are" + leads;
-
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "campaignleads62@gmail.com",
-        Password : "6EBB89F49D89F6CF7D48742A0865352BA966",
-        To : 'campaignleads62@gmail.com',
-        From : "campaignleads62@gmail.com",
-        Subject : "Leads",
-        Body : info
-    }).then(
-      message => {
-        if(message=='OK'){
-            swal("Success", "Leads has been Generated!", "success");
-            console.log(info);
-        }else{
-            swal("Error!", "Error in generating Leads!", "error");
-        }
-      }
-    );
-}
+submit.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    console.log("clicked");
+})
