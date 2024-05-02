4a51c7b288db860feb01237ffc4bf5c4fb6ff1e6
CrazyJaffar <84711098+CrazyJaffar@users.noreply.github.com>
2024-05-02 18:34:17 +0530

    Update Contact form to email.js

--- Contact form to email.js
+++ Contact form to email.js
@@ -1,7 +1,7 @@
 const leads = document.getElementById('Leads');
 const submit = document.getElementById('submit');
 
-const leads = document.getElementByClassName('form contact')[0];
+const leads = document.getElementByClassName('form-contact')[0];
 
 submit.addEventListener('submit',(e)=>{
     e.preventDefault();
