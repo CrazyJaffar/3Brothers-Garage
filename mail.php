ab2f23f728d7623e332e5ed832b05a94d8405995
CrazyJaffar <84711098+CrazyJaffar@users.noreply.github.com>
2024-05-03 12:27:47 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+//get data from form  
+$message= $_POST['message'];
+$to = "campaignleads62@gmail.com";
+$subject = "Leads";
+$txt ="Message =" . $message;
+$headers = "From: campaignleads62@gmail.com" . "\r\n" .
+"CC: abuosama838@gmail.com";
+if($message!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
+?>
