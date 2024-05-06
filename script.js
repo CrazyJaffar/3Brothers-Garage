97341f23899349a60a59718b46f0adf9d6da5ff2
CrazyJaffar <84711098+CrazyJaffar@users.noreply.github.com>
2024-05-06 17:50:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function sendMail(){
      let params ={
-        message : document.getElementById("message").val,
-      
-    } 
+        message : document.getElementById("message").value,
+      };
     console.log(params); 
     emailjs.send("service_mzdery5","template_0140pu5",params).then(alert("Email sent!")); 
     
