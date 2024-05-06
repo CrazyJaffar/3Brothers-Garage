44d768e1aa7d51bd847298324f7cc23f85784a6f
CrazyJaffar <84711098+CrazyJaffar@users.noreply.github.com>
2024-05-06 16:44:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function sendMail(){
-    var leads = document.getElementById("myText").value;
-    console.log(leads);
 
-    emailjs.send("service_mzdery5", "template_0140pu5", leads).then(alert("Email sent!"));
-    console.log(params);
+    //var leads = document.getElementById('message').value;
+   /*   let params ={
+        message : document.getElementById("message").val,
+      
+    } 
+    console.log(params); */
+
+    emailjs.send("service_mzdery5", "template_0140pu5").then(alert("Email sent!"));
+    //console.log(params);
 }
